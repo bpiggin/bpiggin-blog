5b332d08f21f4fa6d6a9734daf86715ee77d7da5
Benjamin Piggin <piggin.ben@gmail.com>
2022-02-25 12:54:37 +0000

    Update GA

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -24,7 +24,7 @@ const siteMetadata = {
     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: 'G-TQKQR8KLG4', // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: 'G-90SCP9Y9DE', // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo
