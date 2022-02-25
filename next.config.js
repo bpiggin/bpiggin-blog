4dd223ed86a898b70f558ad4f38b55b1c4c0b27f
Benjamin Piggin <piggin.ben@gmail.com>
2022-02-25 15:27:45 +0000

    Fix analytics and add mstile

--- next.config.js
+++ next.config.js
@@ -11,7 +11,9 @@ const ContentSecurityPolicy = `
   media-src 'none';
   connect-src *;
   font-src 'self';
-  frame-src giscus.app
+  frame-src giscus.app;
+  script-src: 'unsafe-inline' https://www.googletagmanager.com;
+  img-src: www.googletagmanager.com
 `
 
 const securityHeaders = [
