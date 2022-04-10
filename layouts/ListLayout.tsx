3388c30cca71e5e9531f400eccd25a5c6c2e0776
Benjamin Piggin <piggin.ben@gmail.com>
2022-04-10 21:59:01 +0100

    Fix styles

--- layouts/ListLayout.tsx
+++ layouts/ListLayout.tsx
@@ -32,7 +32,7 @@ export default function ListLayout({ posts, initialDisplayPosts = [], pagination
               type="text"
               onChange={(e) => setSearchValue(e.target.value)}
               placeholder="Search articles"
-              className="block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
+              className="block w-full rounded-md border-none bg-white px-4 py-2 text-gray-900 focus:border-none focus:ring-white dark:bg-gray-800 dark:text-gray-100"
             />
             <svg
               className="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
@@ -53,7 +53,7 @@ export default function ListLayout({ posts, initialDisplayPosts = [], pagination
         <ul>
           {!filteredBlogPosts.length && 'No posts found.'}
           {displayPosts.map((frontMatter) => {
-            const { slug, date, title, summary, tags } = frontMatter
+            const { slug, date, title, summary } = frontMatter
             return (
               <li key={slug} className="py-4">
                 <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
@@ -70,11 +70,6 @@ export default function ListLayout({ posts, initialDisplayPosts = [], pagination
                           {title}
                         </Link>
                       </h3>
-                      <div className="flex flex-wrap">
-                        {tags.map((tag) => (
-                          <Tag key={tag} text={tag} />
-                        ))}
-                      </div>
                     </div>
                     <div className="prose max-w-none text-gray-500 dark:text-gray-400">
                       {summary}
